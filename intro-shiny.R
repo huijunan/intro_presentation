76700d98485e73590f6b765082788df72d60f2f5
huijunan <huijunan@g.ucla.edu>
2020-05-04 23:43:30 -0700

    self-intro part

--- intro-shiny.R
+++ intro-shiny.R
@@ -1,6 +1,8 @@
 library(shiny)
 library(shinydashboard)
 library(shinyjqui)
+library(shinyWidgets)
+
 
 ui <- dashboardPage(
   dashboardHeader(title = "Huijun An Presentation",
@@ -40,9 +42,92 @@ ui <- dashboardPage(
                                           .main-sidebar { font-size: 22px; }
                                           .sidebar-menu li { margin-bottom: 10px;} 
                                           .treeview-menu>li>a { font-size: 18px!important; }
-                                          '))))
+
+                                          .modal.in .modal-dialog{
+                                          width:100%;
+                                          height:100%;
+                                          margin:0px;
+                                          }
+                                          
+                                          .modal-content{
+                                          width:100%;
+                                          height:100%;
+                                          background-color: #045494;
+                                          color: #F4CCCC;
+                                          background-image: url("back.jpg")
+                                          }
+
+                                          '))),
+                tabItems(# First tab content
+                  
+                  tabItem(tabName = "me",
+                          fluidRow(
+                            box(
+                              title = h1("About me"), width = NULL, status = "primary", solidHeader = TRUE,
+                              tags$head(
+                                tags$style(HTML("ul {list-style-type: square;}"))
+                                ),
+                              tags$div(tags$ul(
+                                tags$li(tags$span(h3("Huijun(means wise noble man in Chinese) An"))),
+                                tags$li(tags$span(h3("Expected MS in Biostatistics at UCLA (current GPA 3.87), BS in Biochemistry at UCLA"))),
+                                tags$li(tags$span(h3("Flexible, thoughtful, and detail-oriented individual who look forward to make real-world contribution in healthcare related data analysis")))))
+                            )
+                          )),
+                  
+                  # Second tab content
+                  tabItem(tabName = "exp",
+                          fluidRow(
+                            box(h1("Experience"),width = NULL, status = "primary", solidHeader = TRUE,
+                  fluidRow(
+                    valueBox(500, strong(h4("Million records dealt with")), icon = icon("th")),
+                    valueBox(4, strong(h4("Years of related experience")), icon = icon("calendar"))
+                  ),
+                  tags$img(src='exp.png')
+                  ))
+                ),
+                # Third tab content
+                tabItem(tabName = "skill",
+                        fluidRow(
+                          box(h1("Skills"),width = NULL, status = "primary", solidHeader = TRUE,
+                              tags$img(src='skill.png')
+                          ))
+                ),
+                # fourth tab content
+                tabItem(tabName = "hobby",
+                        fluidRow(
+                          box(h1("Me outside of work..."),width = NULL, status = "primary", solidHeader = TRUE,
+                              tags$head(
+                                tags$style(HTML("ul {list-style-type: square;}"))
+                              ),
+                              tags$div(tags$ul(
+                                tags$li(tags$span(h3("Keyboard player in a metal band"))))
+                                ),
+                              tags$img(src='band.jpg'),
+                              tags$div(tags$ul(
+                                tags$li(tags$span(h3("Proud owner of beautiful one-year-old cat Qiuqiu"))))
+                              ),
+                              tags$img(src='qiu.jpg')
+                          ))
+                )
+                )
+  )
 )
 
-server <- function(input, output) { }
+server <- function(input, output) {
+  # Landing page
+  histdata <- rnorm(500)
+  observeEvent(once = TRUE,ignoreNULL = FALSE, ignoreInit = FALSE, eventExpr = histdata, { 
+    # event will be called when histdata changes, which only happens once, when it is initially calculated
+    showModal(modalDialog(
+      title = "", 
+      h1("Welcome to Huijun An's Personal Introduction"),
+      setBackgroundImage(
+        src = "Rplot.png"
+      )
+    ))
+  })
+  
+  
+}
 
 shinyApp(ui, server)
